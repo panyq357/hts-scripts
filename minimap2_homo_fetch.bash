201e1de7a01cb58ebd0b53a23eb4609d638e6c93
panyq357 <panyq357@qq.com>
2024-10-14 10:57:50 +0800

    simplify minimap2_homo_fetch.bash

--- minimap2_homo_fetch.bash
+++ minimap2_homo_fetch.bash
@@ -1,19 +1,8 @@
+#!/usr/bin/env bash
+
 # Given a position range in ref genome, use minimap2 to fetch homologous sequence in target genome.
-#
-# author: panyq357
-# date: 2023-10-23
 
-out_name="out.fa"      # Prefix of output file.
-chr="chr01"            # Chromosome in ref genome.
-pos_start=$((100000))  # Position in ref genome.
-pos_end=$((101000))
+# Usage: ./minimap2_homo_fetch.bash input.fa target.genome.fa out.fa
 
-ref_genome="path/to/ref.genome.fa" 
-target_genome="path/to/target.genome.fa"
-
-samtools faidx ${ref_genome} ${chr}:${pos_start}-${pos_end} \
-| minimap2 -a ${target_genome} - \
-| samtools view -b \
-| bedtools bamtobed -i stdin \
-| bedtools getfasta -fi ${target_genome} -bed stdin > ${out_name}
+minimap2 -a $2 $1 | samtools view -b | bedtools bamtobed -i stdin | bedtools getfasta -fi $2 -bed stdin > $3
 
